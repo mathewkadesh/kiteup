d91c1efc22986af3d30afb0452cdc17f572965a7
mathewkadesh <kadeshmathew@gmail.com>
2022-08-10 15:26:01 +0530

    Added new files

---
+++ js/functions.js
@@ -0,0 +1,60 @@
+const  openModalButton = document.querySelectorAll('[data-modal-target]')
+const  closeModalButton = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButton.forEach(button =>{
+    button.addEventListener('click',()=>{
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+overlay.addEventListener('click', ()=>{
+    const modals =  document.querySelectorAll('.modal.active')
+    modals.forEach(modal =>{
+        closeModal(modal)
+    })
+})
+
+
+closeModalButton.forEach(button =>{
+    button.addEventListener('click',()=>{
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+// --------------------------------------------------------- Contact US ---------------------------------------------------------
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+});
+
+// --------------------------------------------------------- Animation ---------------------------------------------------------
+
